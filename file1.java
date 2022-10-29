8da33d9f18e0c348e3af37e7cafa0527ab196798
vinay1650 <vinayrocks650@gmail.com>
2022-10-29 11:45:41 +0530

    mm

--- file1.java
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-new file must be created berfore using another file
-we need to understand some basic concepts of networking
-=======
-new file must be created berfore using code
->>>>>>> 36dbb77e847515762737d9404f4ea0349869bddd
--- main.java
+++
@@ -1,1 +0,0 @@
-<new version were updated to see the status of the version>
